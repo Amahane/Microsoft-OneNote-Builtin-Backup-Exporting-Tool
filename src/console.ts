ed82292840ebdb46287a6da20b433fa5083a88c3
天羽 あまはね 中枢机 <Amahane2018@outlook.com>
2019-09-05 17:52:46 +0800

    Console Refactoring Commit

---
+++ src/console.ts
@@ -0,0 +1,120 @@
+import chalk, { Chalk } from 'chalk';
+import readline         from 'readline';
+
+export interface Tag
+{
+    (message : string) : string;
+}
+
+export interface Bullet
+{
+	render(item : string, index : number) : string;
+}
+
+export function createTag(text : string, style : Chalk) : Tag
+{
+	return message => style(`[${ text }] ${ message }`);
+}
+
+export function createBullet(
+	render : (item : string, index : number) => string) : Bullet
+{
+	return { render };
+}
+
+export const prompt    = '> ';
+export const separator = '—'.repeat(60);
+
+export const tags =
+{
+    success     : createTag("成功", chalk.green ),
+    userError   : createTag("错误", chalk.yellow),
+    systemError : createTag("错误", chalk.red   ),
+}
+
+export const bullets =
+{
+	unordered :
+	{
+		default : createBullet((item, index) => '\u2022')
+	},
+	ordered:
+	{
+		default : createBullet((item, index) => `[${ index }]`)
+	}
+};
+
+export function write(message : string) : void
+{
+	process.stdout.write(message);
+}
+
+export function writeLine(message : string) : void
+{
+	process.stdout.write(`${ message }\n`);
+}
+
+export async function question(
+	query     : string  = '',
+	usePrompt : boolean = true) : Promise<string>
+{
+	const i = readline.createInterface(
+	{
+		input    : process.stdin,
+		output   : process.stdout,
+		terminal : true
+	});
+
+	const q = query
+		? usePrompt ? `${ query } ${ prompt }` : query
+		: usePrompt ?                prompt    : '';
+	const s = await new Promise<string>(resolve => i.question(q, answer => resolve(answer)))
+
+	i.close(); return s;
+}
+
+export function writeList(
+	items  : string[],
+	bullet : Bullet = bullets.unordered.default) : void
+{
+	items.forEach((value, index) => writeLine(`${ bullet.render(value, index + 1) } ${ value }`));
+}
+
+export async function questionTillValid(
+	query : string,
+	validateCallback : (input : string) => boolean,
+	errorCalllback   : (input : string) => void =
+		(input) => writeLine(tags.userError(`${ input } 不是有效的输入。`)),
+	usePrompt : boolean = true) : Promise<string>
+{
+	while (true)
+	{
+		const input = await question(query, usePrompt);
+		if (validateCallback(input)) return input;
+		errorCalllback(input);
+	}
+}
+
+export async function writeAndQuestionChoice(
+	intro     : string  ,
+	items     : string[],
+	query     : string  ,
+	usePrompt : boolean = true) : Promise<string>
+{
+	writeLine(intro);
+	writeList(items, bullets.ordered.default);
+
+	const input = await questionTillValid(
+		usePrompt ? `${ query }(输入序号) ${ prompt }` : query,
+		input =>
+		{
+			if (! input) return false;
+			const index = Number(input);
+			if (! Number.isInteger(index)) return false;
+			return index >= 1 && index <= items.length;
+		},
+		input => writeLine(tags.userError(`\"${ input }\" 不是有效的序号。`)),
+		false);
+
+	return items[Number(input) - 1];
+}
--- src/main.ts
+++ src/main.ts
@@ -1,227 +1,187 @@
-import fs       from 'fs-extra';
-import path     from 'path';
-import assert   from 'assert';
-import chalk    from 'chalk'
-import readline from 'readline';
+import fs      from 'fs-extra';
+import path    from 'path';
+import chalk from 'chalk';
 
-function range(begin : number, end : number) : number[]
+import * as console from './console';
+
+const appInfo =
 {
-	return [...(function *() : IterableIterator<number>
-	{
-		for (let i = begin; i < end; ++ i) yield i;
-	})()];
-}
-
-const console =
-{
-	write(s : string) : void
-	{
-		process.stdout.write(s);
-	},
-
-	writeLine(s : string) : void
-	{
-		process.stdout.write(`${ s }\n`);
-	},
-
-	writeError(s : string) : void
-	{
-		process.stdout.write(chalk.red(`[错误] ${ s }\n`));
-	},
-
-	async question(query : string) : Promise<string>
-	{
-		const i = readline.createInterface(
-		{
-			input    : process.stdin,
-			output   : process.stdout,
-			terminal : true
-		});
-
-		const s = await new Promise<string>(
-			resolve => i.question(query, answer => resolve(answer)))
-
-		i.close();
-
-		return s;
-	},
-
-	writeChoices(choices : string[]) : void
-	{
-		choices.forEach((value, index) => value && this.writeLine(`[${ index + 1 }] ${ value }`));
-	},
-
-	async questionChoice(choices : string[], query : string, queryRetry : string) : Promise<string>
-	{
-		let retry = false;
-		while (true)
-		{
-			const choice = await this.question(retry ? queryRetry : query);
-			if (choices.find(value => value === choice)) return choice;
-			retry = true;
-		}
-	}
+	title : "Microsoft OneNote Builtin Backup Exporting Tool"
 };
 
-(async () =>
+const backupRoot = path.resolve(process.env.LOCALAPPDATA!, "./Microsoft/OneNote/16.0/备份/")
+
+async function scanNotebooks() : Promise<string[] | undefined>
 {
-	console.writeLine("Microsoft OneNote Builtin Backup Exporting Tool");
-	console.writeLine('-'.repeat(60));
+	console.writeLine("正在检测可以导出的笔记本...");
 
-	console.write("正在检测可以导出的笔记本 ...");
-
-	const backupRoot = path.resolve(process.env.LOCALAPPDATA!, "./Microsoft/OneNote/16.0/备份/")
-	let notebooks : string[] = [];
+	let notebooks : string[] | undefined;
 	try
 	{
 		notebooks = await fs.readdir(backupRoot);
+		if (notebooks.length === 0)
+		{
+			console.writeLine(console.tags.userError("[提示] 当前 Microsoft OneNote 2016 备份文件中没有任何备份的笔记本 。"));
+			return;
+		}
+		else return notebooks;
 	}
 	catch
 	{
-		console.writeError("无法定位 Microsoft OneNote 2016 备份文件 。");
-		process.exit();
-	}
-
-	if (notebooks.length === 0)
-	{
-		console.writeLine(chalk.yellow("[信息] 当前 Microsoft OneNote 2016 备份文件中没有任何备份的笔记本 。 "));
+		console.writeLine(console.tags.systemError("无法定位 Microsoft OneNote 2016 备份文件 。"));
 		return;
 	}
+};
 
-	console.writeLine("发现以下备份的笔记本可以导出 ：");
-	console.writeChoices(notebooks);
-	const notebook = notebooks[Number(await console.questionChoice(
-		range(1, notebooks.length + 1).map(i => i.toString()),
-		"请选择需要导出的笔记本（ 输入编号 ）> ",
-		`${ chalk.red("无效选项。") }请重新选择需要导出的笔记本（ 输入编号 ）> `)) - 1];
+async function questionNotebook(notebooks : string[]) : Promise<string>
+{
+	return await console.writeAndQuestionChoice(
+		"发现以下备份的笔记本可以导出:",
+		notebooks,
+		"请选择需要导出的笔记本");
+}
 
-	interface Section
+class Section
+{
+	constructor(
+		public notebook  : string,
+		public hierarchy : string[],
+		public name      : string,
+		public backup    : string) { }
+
+	public get fullName () : string
 	{
-		notebook : string;
-		hierarchy : string[];
-		name : string;
-		backup : string;
+		return this.hierarchy.concat([ this.name ]).join('/');
 	}
 
-	function getSectionPath(section : Section) : string
+	public async export (destinationRoot : string) : Promise<void>
+	{
+		await fs.copy(this.getSourcePath(), this.getDestinationPath(destinationRoot));
+	}
+
+	public getSourcePath() : string
 	{
 		return path.resolve(
-			backupRoot, section.notebook, ...section.hierarchy,
-			`${ section.name }.one (于 ${ section.backup }).one`);
+			backupRoot,
+			this.notebook,
+			... this.hierarchy,
+			`${ this.name }.one (于 ${ this.backup }).one`);
 	}
 
-	function getSectionFullName(section : Section) : string
+	public getDestinationPath(destinationRoot : string) : string
 	{
-		return `${ section.hierarchy.join(' / ') } / ${ section.name }`;
+		return path.resolve(
+			destinationRoot,
+			this.notebook,
+			... this.hierarchy,
+			`${ this.name }.one`);
 	}
+}
 
-	async function searchSections(hierarchy : string[]) : Promise<Section[]>
+async function scanSections(
+	notebook  : string,
+	hierarchy : string[] = []) : Promise<Section[]>
+{
+	let sections : Section[] = [];
+	for (const fileOrSubdir of await fs.readdir(path.resolve(backupRoot, notebook, ... hierarchy)))
 	{
-		let sections : Section[] = [];
-		for (const fileOrSubdir of await fs.readdir(path.resolve(backupRoot, notebook, ... hierarchy)))
+		if ((await fs.stat(path.resolve(backupRoot, notebook, ... hierarchy, fileOrSubdir))).isDirectory())
 		{
-			if ((await fs.stat(path.resolve(backupRoot, notebook, ... hierarchy, fileOrSubdir))).isDirectory())
+			sections = [ ... sections, ... await scanSections(notebook, [... hierarchy, fileOrSubdir])];
+		}
+		else
+		{
+			if (fileOrSubdir.endsWith(".one"))
 			{
-				sections = [ ... sections, ... await searchSections([... hierarchy, fileOrSubdir])];
-			}
-			else
-			{
-				if (fileOrSubdir.endsWith(".one"))
-				{
-					const [ name, backupAndSuffix ] = fileOrSubdir.split(".one (于 ");
-					const [ backup ] = backupAndSuffix.split(").one");
-					sections.push(
-					{
-						notebook : notebook,
-						hierarchy : hierarchy,
-						name : name,
-						backup : backup
-					})
-				}
+				const [ name, backupAndSuffix ] = fileOrSubdir.split(".one (于 ");
+				const [ backup ] = backupAndSuffix.split(").one");
+				sections.push(
+				new Section(notebook, hierarchy, name, backup))
 			}
 		}
-		return sections;
 	}
+	return sections;
+}
 
-	const scannedSections = await searchSections([]);
+async function questionDestinationRoot() : Promise<string | undefined>
+{
+	while (true)
+	{
+		const destinationRoot = await console.question("请输入导出目录，目录中不需包含笔记本名");
+		if (! path.isAbsolute(destinationRoot))
+		{
+			console.writeLine(chalk.red("以上目录不是绝对路径。") + "需要绝对路径 ，请重试 >")
+			continue;
+		}
+		try
+		{
+			await fs.access(destinationRoot);
+			return destinationRoot;
+		}
+		catch
+		{
+			const yesno = await console.questionTillValid(
+				`无法确认目录 \"${ destinationRoot }\"是否存在，是否尝试创建此目录？输入y以继续 ，或输入n以退出`,
+				input => input === "y" || input === "n");
+			if (yesno === "n") return;
+
+			try
+			{
+				await fs.mkdir(destinationRoot);
+				return destinationRoot;
+			}
+			catch
+			{
+				console.writeLine(console.tags.systemError(`目录创建失败 ，\"${ destinationRoot }\" 可能不是有效的目录。`));
+				return;
+			}
+		}
+	}
+}
+
+(async () =>
+{
+	console.writeLine(appInfo.title);
+	console.writeLine(console.separator);
+
+	const notebooks : string[] | undefined = await scanNotebooks();
+	if (! notebooks) return;
+	const notebook = await questionNotebook(notebooks);
+
+	const sectionsOfAllNotebook = await scanSections(notebook);
 	const backups  = (() : string[] =>
 	{
 		let backups : string[] = [];
-		scannedSections.forEach(section =>
+		sectionsOfAllNotebook.forEach(section =>
 		{
 			if (backups.find(backup => backup === section.backup) === undefined)
 				backups.push(section.backup);
 		})
 		return backups;
 	})();
-	console.writeLine(`准备导出笔记本 \"${ notebook }\"。这个笔记本存在以下备份版本 ：`);
-	console.writeChoices(backups);
-	const backup = backups[Number(await console.questionChoice(
-		range(1, backups.length + 1).map(i => i.toString()),
-		"请选择需要导出的备份（ 输入编号 ）> ",
-		`${ chalk.red("无效选项。") }请重新选择需要导出的备份（ 输入编号 ）> `)) - 1];
+	const backup = await console.writeAndQuestionChoice(
+		`准备导出笔记本 \"${ notebook }\"。这个笔记本存在以下备份版本:`,
+		backups,
+		"请选择需要导出的备份"
+	);
 
-	const filteredSections = scannedSections.filter(section => section.backup === backup);
-	console.writeLine("即将导出以下分区的备份 ：");
-	console.writeLine(filteredSections.map<string>(section => `\t${ getSectionFullName(section) }`).join('\n'));
+	const filteredSections = sectionsOfAllNotebook.filter(section => section.backup === backup);
+	console.writeLine("即将导出以下分区的备份:");
+	console.writeList(filteredSections.map<string>(section => section.fullName));
 
+	let destinationRoot = await questionDestinationRoot();
+	if (! destinationRoot) return;
 
-	let destination = await (async () : Promise<string | undefined> =>
-	{
-		while (true)
-		{
-			const destination = await console.question("请输入导出目录，目录中不需包含笔记本名 > ");
-			if (! path.isAbsolute(destination))
-			{
-				console.writeLine(chalk.red("以上目录不是绝对路径。") + "需要绝对路径 ，请重试 >")
-				continue;
-			}
-			try
-			{
-				await fs.access(destination);
-				return destination;
-			}
-			catch
-			{
-				const yesno = await console.questionChoice(
-					['y', 'n'],
-					`无法确认目录 \"${ destination }\"是否存在，是否尝试创建此目录？输入 y 以继续 ，或输入 n 以退出 > `,
-					`${ chalk.red("不是有效的输入。") } 输入 y 以继续 ，或输入 n 以退出 >`);
-				if (yesno === "n") return;
-
-				try
-				{
-					await fs.mkdir(destination);
-					return destination;
-				}
-				catch
-				{
-					console.writeError(`目录创建失败 ，\"${ destination }\" 可能不是有效的目录。`);
-					return;
-				}
-			}
-		}
-	})();
-	if (! destination) return;
-
-	console.writeLine(`已经准备好将笔记本 ${ notebook } 的 ${ backup } 备份导出至目录 \"${ destination }\" 下的 ${ notebook } 文件夹中。`)
+	console.writeLine(`已经准备好将笔记本 ${ notebook } 的 ${ backup } 备份导出至目录 \"${ destinationRoot }\" 下的 ${ notebook } 文件夹中。`)
 	await console.question(chalk.yellow(`按下 Enter 开始导出 ，注意导出完成前强行终止程序可能导致不完整的导出 。`))
-
-	function getSectionDestinationPath(section : Section) : string
-	{
-		return path.resolve(
-			destination!, section.notebook, ...section.hierarchy,
-			`${ section.name }.one`);
-	}
 
 	let sectionCopied = 0;
 	const actions : Promise<void>[] = filteredSections.map(async section =>
 	{
-		await fs.copy(getSectionPath(section), getSectionDestinationPath(section));
-		console.writeLine(chalk.green(
-			`[成功] [第 ${ ++ sectionCopied } 个 / 共 ${ filteredSections.length } 个] 分区 ${ getSectionFullName(section) } 已经完成导出 ！`));
+		await section.export(destinationRoot !);
+		console.writeLine(console.tags.success(`[第${ ++ sectionCopied }个/共${ filteredSections.length }个] 分区 \"${ section.fullName }\" 已经完成导出 ！`));
 	});
 	await Promise.all(actions);
 
-	console.writeLine(chalk.green("[成功] 所有分区已经完成导出。"));
+	console.writeLine(console.tags.success("所有分区已经完成导出。"));
 })()
